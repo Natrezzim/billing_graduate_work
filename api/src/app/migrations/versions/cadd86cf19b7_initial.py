d67909b15696152a15f1e21b1fac86716c47a31f
mkezikov <mkezikov@sdvor.com>
2022-07-17 14:24:22 +0500

    изменил структуру бд

--- api/src/app/migrations/versions/cadd86cf19b7_initial.py
+++ api/src/app/migrations/versions/cadd86cf19b7_initial.py
@@ -19,27 +19,60 @@ depends_on = None
 
 payment_platforms = sa.Enum('yookassa', name='payment_platform_enum')
 payment_status = sa.Enum('pending', 'succeeded', 'canceled', 'waiting_for_capture', name='payment_status_enum')
+currencies = sa.Enum('RUB', 'USD', name='currencies_enum')
 
 
 def upgrade() -> None:
     op.create_table(
         'payments',
         sa.Column('id', postgresql.UUID(as_uuid=True), primary_key=True, nullable=False, default=uuid4),
+        sa.Column('cart_id', postgresql.UUID(as_uuid=True), sa.ForeignKey('payments.id'), nullable=False),
         sa.Column('idempotence_uuid', postgresql.UUID(as_uuid=True), nullable=False),
-        sa.Column('amount', sa.JSON, nullable=False),
         sa.Column('description', sa.Text, nullable=False),
         sa.Column('payment_system', payment_platforms, nullable=False),
-        sa.Column('created_at', sa.DateTime, server_default=sa.func.current_timestamp()),
-
+        sa.Column('created_at', sa.TIMESTAMP, server_default=sa.func.now()),
     )
     op.create_table(
         'status',
         sa.Column('id', postgresql.UUID(as_uuid=True), primary_key=True, nullable=False, default=uuid4),
         sa.Column('payment_id', postgresql.UUID(as_uuid=True), sa.ForeignKey('payments.id'), nullable=False),
         sa.Column('status', payment_status, nullable=False),
-        sa.Column('created_at', sa.DateTime, server_default=sa.func.current_timestamp()),
+        sa.Column('created_at', sa.TIMESTAMP, server_default=sa.func.now()),
         sa.Column('paid', sa.Boolean, default=False),
         sa.Column('sync', sa.Boolean, default=False)
+    )
+    op.create_table(
+        'products',
+        sa.Column('id', postgresql.UUID(as_uuid=True), primary_key=True, nullable=False, default=uuid4),
+        sa.Column('name', sa.VARCHAR(100), nullable=False),
+        sa.Column('created_at', sa.TIMESTAMP, nullable=False, server_default=sa.func.now()),
+        sa.Column('updated_at', sa.TIMESTAMP, nullable=False,
+                  server_default=sa.text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')),
+    )
+    op.create_table(
+        'prices',
+        sa.Column('id', postgresql.UUID(as_uuid=True), primary_key=True, nullable=False, default=uuid4),
+        sa.Column('product_id', postgresql.UUID(as_uuid=True), sa.ForeignKey('products.id'), nullable=False),
+        sa.Column('value', sa.Float, asdecimal=True, nullable=False),
+        sa.Column('currency', currencies, nullable=False),
+        sa.Column('created_at', sa.TIMESTAMP, nullable=False, server_default=sa.func.now()),
+        sa.Column('updated_at', sa.TIMESTAMP, nullable=False,
+                  server_default=sa.text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')),
+    )
+    op.create_table(
+        'cart',
+        sa.Column('id', postgresql.UUID(as_uuid=True), primary_key=True, nullable=False, default=uuid4),
+        sa.Column('username', sa.VARCHAR(100), nullable=False),
+        sa.Column('created_at', sa.DateTime, server_default=sa.func.current_timestamp()),
+    )
+    op.create_table(
+        'products_to_cart',
+        sa.Column('id', postgresql.UUID(as_uuid=True), primary_key=True, nullable=False, default=uuid4),
+        sa.Column('product_id', postgresql.UUID(as_uuid=True), sa.ForeignKey('products.id'), nullable=False),
+        sa.Column('cart_id', postgresql.UUID(as_uuid=True), sa.ForeignKey('cart.id'), nullable=False),
+        sa.Column('username', sa.VARCHAR(100), nullable=False),
+        sa.Column('created_at', sa.DateTime, server_default=sa.func.current_timestamp()),
+        sa.UniqueConstraint('product_id', 'cart_id', name='product_cart_uix')
     )
 
 
