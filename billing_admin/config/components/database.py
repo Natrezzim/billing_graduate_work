07d5bb487a23716fc9a3eefd9146e87ca8e7653c
Yuri N. Matveev <ynmatveev@yandex.ru>
2022-07-20 20:34:17 +0300

    Обновлены настройки Django для подключения к БД

--- billing_admin/config/components/database.py
+++ billing_admin/config/components/database.py
@@ -3,11 +3,11 @@ import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
+        'NAME': os.getenv('DB_NAME_ADMIN'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'HOST': os.getenv('DB_HOST_ADMIN', '127.0.0.1'),
+        'PORT': os.getenv('DB_PORT_ADMIN', '5432'),
         'OPTIONS': {
             'options': '-c search_path=public,billing'
         }
